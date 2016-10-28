0a3e7fbe6b215e9d0dfb0c34f3bc2d0ead52b0df
Mike Atencio's <Intencio@Mikes-MBP.attlocal.net>
2016-10-28 15:40:00 -0500

    quanitity check  query

--- Bamazoncustomers.js
+++ Bamazoncustomers.js
@@ -1,6 +1,5 @@
 // The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
-
 // Then create a Node application called BamazonCustomer.js. 
 //first display all of the items available for sale. 
 //Include the ids, names, and prices of products for sale.
@@ -13,59 +12,47 @@ var connection = mysql.createConnection({
    password: "", //Your password
    database: "Bamazon_db"
 })
-
-connection.connect(function(err) {
-   if (err) throw err;
-   console.log("connected as id " + connection.threadId);
-})
-
-
+connection.connect(function(err){})
 connection.query('SELECT * FROM products', function(err, res) {
    if (err) throw err;
    console.log(JSON.stringify(res, null, 2));
 })
-
-
 // The app should then prompt users with two messages.
 // The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
-
 inquirer.prompt([
-
     {
         type: "input",
         message: "What would you like to buy?",
         name: "product"
     },
-
-
     {
         type: "input",
         message: "How much?",
         name: "quantity"
     }
-
 ]).then(function (order) {
-    
-
-    // If we log that user as a JSON, we can see how it looks.
-    console.log(order);
-});
 // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
 // However, if your store does have enough of the product, you should fulfill the customer's order.
+var selected = order.product
+console.log(order.product)
+connection.query("SELECT * FROM products WHERE ?", [{
+   item_id: selected
+}], function(err, res) {
+	 if (err) throw err;
+   var order = JSON.stringify(res, null, 2);
+   console.log(order);
+});
 
-connection.query('SELECT * FROM products', function(err, res) {
-   if (err) throw err;
-   console.log(JSON.stringify(res, null, 2));
-})
 
 
+// if (){
+// }
+// else () {
+// }
 
-if (){
-}
-else () {
-}
+});
 
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
